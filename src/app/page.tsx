7735fab30c58ae078222566ddd5f514c3bd0e76d
HyAgOsK <hyagovieira@geb.inatel.br>
2025-08-05 22:39:07 -0300

    chore: add update shadcn theme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ import { Button } from "@/components/ui/button";
 
 const Home = () => {
   return (
-    <div className="flex h-screen flex-col items-center justify-center">
-      <Button className="bg-black px-5 text-cyan-500">Bootcamp</Button>
+    <div>
+      <Button>Bootcamp</Button>
     </div>
   );
 };

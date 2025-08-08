2d5c7b387b41b5f6b763e63d93b073cc9a93beb0
HyAgOsK <hyagovieira@geb.inatel.br>
2025-08-08 15:41:34 -0300

    feat: add product list and product item components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,46 @@
-import { Header } from "@/components/common/header";
-import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
-const Home = () => {
+import { Header } from "@/components/common/header";
+import ProductList from "@/components/common/product-list";
+import { db } from "@/db";
+
+const Home = async () => {
+  const products = await db.query.productTable.findMany({
+    with: {
+      variants: true,
+    },
+  });
+
+  console.log("Products found:", products.length);
+  console.log("First product:", products[0]);
+
   return (
     <>
       <Header />
+      <div className="pg-5 space-y-6">
+        <div className="px-5">
+          <Image
+            src="/banner01.png"
+            alt="leve uma vida com estilo"
+            height={0}
+            width={0}
+            sizes="100vw"
+            className="h-auto w-full"
+          />
+        </div>
+
+        <ProductList products={products} title="Mais Vendidos" />
+        <div className="px-5">
+          <Image
+            src="/banner02.png"
+            alt="leve uma vida com estilo"
+            height={0}
+            width={0}
+            sizes="100vw"
+            className="h-auto w-full"
+          />
+        </div>
+      </div>
     </>
   );
 };

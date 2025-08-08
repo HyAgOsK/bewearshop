6313e1208c605016d91ecb015be52a5ad50d0c2a
HyAgOsK <hyagovieira@geb.inatel.br>
2025-08-08 20:51:17 -0300

    feat: add category products page

--- src/components/common/product-item.tsx
+++ src/components/common/product-item.tsx
@@ -3,11 +3,13 @@ import Link from "next/link";
 
 import { productTable, productVariantTable } from "@/db/schema";
 import { formatCentsToBRL } from "@/helpers/money";
+import { cn } from "@/lib/utils";
 
 interface ProductItemProps {
   product: typeof productTable.$inferSelect & {
     variants: (typeof productVariantTable.$inferSelect)[];
   };
+  textContainerClassName?: string;
 }
 
 // Product images mapping from seeds
@@ -166,7 +168,7 @@ const productImages = {
   },
 };
 
-const ProductItem = ({ product }: ProductItemProps) => {
+const ProductItem = ({ product, textContainerClassName }: ProductItemProps) => {
   const firstVariant = product.variants[0];
 
   // Get the correct image URL from our mapping
@@ -193,11 +195,17 @@ const ProductItem = ({ product }: ProductItemProps) => {
       <Image
         src={imageUrl}
         alt={firstVariant?.name || product.name}
-        width={200}
-        height={200}
-        className="rounded-3xl"
+        sizes="100vw"
+        width={0}
+        height={0}
+        className="h-auto w-full rounded-3xl"
       />
-      <div className="flex max-w-[200px] flex-col gap-1">
+      <div
+        className={cn(
+          "flex max-w-[200px] flex-col gap-1",
+          textContainerClassName,
+        )}
+      >
         <p className="truncate text-sm font-medium">{product.name}</p>
         <p className="text-muted-foreground truncate text-xs font-medium">
           {product.description}
